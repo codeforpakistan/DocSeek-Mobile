053d47510d6e91597967346669ddf96595c2f2c7
wingoku <ushersyoutube@yahoo.com>
2014-04-13 00:18:41 +0500

    updates

---
+++ src/com/wingoku/docseek/SplashScreen_Frag.java
@@ -0,0 +1,18 @@
+package com.wingoku.docseek;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class SplashScreen_Frag extends SherlockFragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.splash_screen_frag, container, false);
+	}
+}
